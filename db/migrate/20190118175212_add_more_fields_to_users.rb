65302b39f3e00b52b59c36b09a88252884333409
Cesare Camurani <cesare.camurani@gmail.com>
2019-02-19 11:41:08 +0000

    hashtags added to posts

--- db/migrate/20190118175212_add_more_fields_to_users.rb
+++ db/migrate/20190118175212_add_more_fields_to_users.rb
@@ -1,6 +1,5 @@
 class AddMoreFieldsToUsers < ActiveRecord::Migration[5.2]
   def change
-    add_column :users, :username, :string
     add_column :users, :name, :string
     add_column :users, :website, :string
     add_column :users, :bio, :text
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_19_102433) do
+ActiveRecord::Schema.define(version: 2019_02_19_112556) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,21 @@ ActiveRecord::Schema.define(version: 2019_02_19_102433) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
+  create_table "hash_tags", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "post_hash_tags", force: :cascade do |t|
+    t.bigint "post_id"
+    t.bigint "hash_tag_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["hash_tag_id"], name: "index_post_hash_tags_on_hash_tag_id"
+    t.index ["post_id"], name: "index_post_hash_tags_on_post_id"
+  end
+
   create_table "posts", force: :cascade do |t|
     t.string "description"
     t.integer "user_id"
@@ -44,6 +59,7 @@ ActiveRecord::Schema.define(version: 2019_02_19_102433) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "username", default: "", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
@@ -55,7 +71,6 @@ ActiveRecord::Schema.define(version: 2019_02_19_102433) do
     t.string "website"
     t.text "bio"
     t.string "avatar"
-    t.string "username", default: "", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
